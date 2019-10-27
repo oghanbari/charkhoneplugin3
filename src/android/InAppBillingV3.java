44dcbff80c2ca67a622271638fcaf512707a956c
Omid Ghanbari <oghanbari@live.com>
2019-10-27 21:30:51 +0330

    Update InAppBillingV3.java

--- src/android/InAppBillingV3.java
+++ src/android/InAppBillingV3.java
@@ -65,7 +65,7 @@ public class InAppBillingV3 extends CordovaPlugin {
         Context context = this.cordova.getActivity();
         InputStream is;
         try {
-            is = context.getAssets().open("www/manifest.json");
+            is = context.getAssets().open("www/my-manifest.json");
             Scanner s = new Scanner(is).useDelimiter("\\A");
             String manifestString = s.hasNext() ? s.next() : "";
             Log.d(TAG, "manifest:" + manifestString);
